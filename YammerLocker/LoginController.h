a0d7089496dfa766c7f9d24f74846f29ea6a673b
Sidd Singh <ssingh@yammer-inc.com>
2013-08-12 17:37:14 -0700

    Removing NXOauth2 client, adding own login controller

---
+++ YammerLocker/LoginController.h
@@ -0,0 +1,13 @@
+//
+//  YammerLoginController.h
+//  YammerLocker
+//
+//  Created by Sidd Singh on 7/16/13.
+//  Copyright (c) 2013 Sidd Singh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YammerLoginController : NSObject
+
+@end
---
+++ YammerLocker/LoginController.m
@@ -0,0 +1,13 @@
+//
+//  YammerLoginController.m
+//  YammerLocker
+//
+//  Created by Sidd Singh on 7/16/13.
+//  Copyright (c) 2013 Sidd Singh. All rights reserved.
+//
+
+#import "YammerLoginController.h"
+
+@implementation YammerLoginController
+
+@end
